f0478f231d2bca65390937d50b6540fce59cb399
Anton Dyshkant <vyshkant@gmail.com>
2018-04-17 04:46:38 +0300

    Added TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for react-native-exit-app 1.0.0
+// Project: https://github.com/wumke/react-native-exit-app
+// Definitions by: Anton Dyshkant <https://github.com/vyshkant>
+
+declare module 'react-native-exit-app' {
+
+    const RNExitApp: {
+        exitApp: () => void
+    };
+
+    export = RNExitApp;
+}
