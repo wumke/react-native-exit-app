1cc1b51b5bae18baa1e30d418caf4ce911053f9e
Aakash Sigdel <aakash.sigdel@gmail.com>
2020-03-03 13:37:02 +0100

    conditional RCTBridgeModule import

--- ios/RNExitApp/RNExitApp.m
+++ ios/RNExitApp/RNExitApp.m
@@ -1,5 +1,10 @@
 #import <UIKit/UIKit.h>
+#if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
 
 
 @interface RNExitApp : NSObject <RCTBridgeModule>
