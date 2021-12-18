a50088a13a1f560f88acb014d4e63ca505f2f0f1
abing <abing@gcores.com>
2021-12-18 13:51:42 +0800

    fix: change iOS react header import style to work with Expo SDK

--- ios/RNExitApp/RNExitApp.m
+++ ios/RNExitApp/RNExitApp.m
@@ -1,10 +1,5 @@
 #import <UIKit/UIKit.h>
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
-
 
 
 @interface RNExitApp : NSObject <RCTBridgeModule>
