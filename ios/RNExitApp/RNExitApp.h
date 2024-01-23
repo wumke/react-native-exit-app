c8bef64d075d4d24f6d41e40ce376d2f3d1de0f4
Francesco-Voto <francesco.voto@nearform.com>
2024-01-23 09:56:38 +0100

    fix: import tight codegen spec on header file

--- ios/RNExitApp/RNExitApp.h
+++ ios/RNExitApp/RNExitApp.h
@@ -7,7 +7,7 @@
 #endif
 
 #if RCT_NEW_ARCH_ENABLED
-#import <React-Codegen/RNExitAppSpec/RNExitAppSpec.h>
+#import <RNExitAppSpec/RNExitAppSpec.h>
 #endif
 
 @interface RNExitApp : NSObject <RCTBridgeModule>
